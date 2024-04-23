1588460199a274a8793b33e683aee6bb53458558
Oleg Karlin <o.karlin1981@gmail.com>
2024-04-23 14:26:39 +0300

    add Sem2:Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задание 1. Совместная работа
+
+// Напишите программу, которая принимает на вход трёхзначное число и удаляет вторую
+// цифру этого числа.
+
+// Примеры:
+// a = 256 => 26
+// a = 891 => 81
+
+Console.WriteLine("Введите целое положительное трёхзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num >= 100 && num <= 999)
+{
+    int firstDigit = num / 100;
+    int thirdDigit = num % 10;
+
+    int result = firstDigit * 10 + thirdDigit;
+
+    Console.WriteLine(result);
+}
+else
+{
+    Console.WriteLine("Некорректный ввод!");
+}
